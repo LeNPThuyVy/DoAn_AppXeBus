c5eb8ff99b6b4620f3c3ce9c7056bf73659c1ea8
LeNPThuyVy <lengocphuongthuy110205@gmail.com>
2025-08-25 10:43:50 +0700

    feature/them cap phep may anh va thu vien anh

--- FaceScanScreen.tsx
+++ FaceScanScreen.tsx
@@ -1,8 +1,52 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React,{useState} from "react";
+import { Text, TouchableOpacity, View,Alert } from "react-native";
 import styles from "./styles"; // dùng file styles.ts chung
+import * as ImagePicker from "expo-image-picker";
 
 export default function FaceScan() {
+  //Xin cấp quyền ảnh
+  const [selectedImg,setSelectedImg]=useState<string|null>(null);
+  const takeImg=async()=>
+  {
+  const {status} =await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+  if(status!="granted")
+  {
+      Alert.alert("Permission was denied!","Function needs to be allowed to connect the gallery!");
+      return;
+  }
+
+  let result=await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:ImagePicker.MediaTypeOptions.Images,
+      allowsEditing:true,
+      quality:1,
+  });
+
+  if(!result.canceled)
+  {
+      setSelectedImg(result.assets[0].uri)
+  }
+  }
+
+  //Xin cấp quyền camera
+  const [takePhoto,setTakePhoto]=useState<string|null>(null);
+  const takePicture=async()=>
+  {
+    const {status}=await ImagePicker.requestCameraPermissionsAsync();
+    if(status!="granted")
+    {
+      Alert.alert("Permission was denied!","Function needs to be allowed to connect the gallery!");
+      return;
+    }
+    let result=await ImagePicker.launchCameraAsync({
+        allowsEditing:true,
+        quality:1,
+    })
+    if(!result.canceled)
+    {
+      setTakePhoto(result.assets[0].uri);
+    }
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Quét gương mặt</Text>
@@ -11,11 +55,17 @@ export default function FaceScan() {
 
       <Text style={styles.instruction}>Hãy đưa khuôn mặt vào trong khung</Text>
 
-      <TouchableOpacity style={styles.buttonFS}>
+      <TouchableOpacity
+       style={styles.buttonFS}
+       onPress={takePicture}
+      >
         <Text style={styles.buttonTextFS}>Bắt đầu</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.buttonFS}>
+      <TouchableOpacity
+       style={styles.buttonFS}
+       onPress={takeImg}
+      >
         <Text style={styles.buttonTextFS}>Tải ảnh lên</Text>
       </TouchableOpacity>
     </View>
